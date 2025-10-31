adb376925caa301a320af2258dccf9f9c32195f0
Eric Yuan <yuaneric6@gmail.com>
2025-10-31 12:40:24 -0400

    10/31
    did NOT publish api key

--- src/hidden.js
+++
@@ -1,3 +0,0 @@
-const runwareApiKey = "d3FslJtaEWFfHT8GVo5c4YCx1KruQ3YI";
-
-export default runwareApiKey;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.jsx'
 import About from './components/about/About.jsx'
-import CatCam from './components/cat_cam/CatCam.jsx'
 import BlogRoutes from './components/blog/BlogRoutes.jsx'
 import Game from './components/game/Game.jsx'
 
@@ -11,7 +10,6 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />} />
-      <Route path="/cat-cam" element={<CatCam />} />
       <Route path="/game" element={<Game />} />
       {BlogRoutes()}
     </Routes>
